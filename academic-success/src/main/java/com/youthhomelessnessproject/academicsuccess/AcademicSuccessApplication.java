89173d8a7215102cd95a0eb86bd1a8d5298544c3
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-06-09 12:51:42 -0400

    Initial commit.

---
+++ academic-success/src/main/java/com/youthhomelessnessproject/academicsuccess/AcademicSuccessApplication.java
@@ -0,0 +1,13 @@
+package com.youthhomelessnessproject.academicsuccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicSuccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicSuccessApplication.class, args);
+    }
+
+}
---
+++ academic-success/src/test/java/com/youthhomelessnessproject/academicsuccess/AcademicSuccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youthhomelessnessproject.academicsuccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicSuccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
