6727fac26ac1a3bbac26075c2b6c803c6c70abad
jamesandrewpurvis <84031772+jamesandrewpurvis@users.noreply.github.com>
2022-07-21 21:59:59 -0600

    Create mockdata.sql
    experimenting with mock data

---
+++ mockdata.sql
@@ -0,0 +1,55 @@
+<-- EMPLOYEE MOCK DATA -- DEFAULT PASS is employee>
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (1, 'Vern', 'Baggaley', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'vbaggaley0');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (2, 'Allen', 'Shark', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'ashark1');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (3, 'Cherie', 'Halfhead', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'chalfhead2');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (4, 'Sacha', 'Rosenkrantz', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'srosenkrantz3');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (5, 'Kerwinn', 'Gorriessen', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'kgorriessen4');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (6, 'Nicolais', 'Balint', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'nbalint5');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (7, 'Nichole', 'O''Doogan', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'nodoogan6');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (8, 'Nancie', 'Elvins', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'nelvins7');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (9, 'Mozes', 'Keeffe', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'mkeeffe8');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (10, 'Piggy', 'Foltin', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'pfoltin9');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (11, 'Waylon', 'Pietzner', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'wpietznera');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (12, 'Jasen', 'De Pero', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'jdeperob');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (13, 'Patrice', 'Twinbourne', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'ptwinbournec');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (14, 'Hillary', 'Ubsdall', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'hubsdalld');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (15, 'Avrom', 'Kelway', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'akelwaye');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (16, 'Abbe', 'Geall', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'ageallf');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (17, 'Leanna', 'McMeyler', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'lmcmeylerg');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (18, 'Wade', 'Birds', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'wbirdsh');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (19, 'Ambrosi', 'Nuzzti', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'anuzztii');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (20, 'Tymothy', 'Hoggan', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'thogganj');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (21, 'Adria', 'Logsdail', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'alogsdailk');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (22, 'Ashby', 'Earley', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'aearleyl');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (23, 'Kingsly', 'Siaspinski', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'ksiaspinskim');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (24, 'Melly', 'McWard', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'mmcwardn');
+insert into tbl_employees (id, first_name, last_name, password, user_role, username) values (25, 'Virgie', 'Stratford', '$2a$12$vELJebV6s62kIbR1V..tXO6SAPHqYz5PcG4gWtNley1mUDH1oTtCm', 'employee', 'vstratfordo');
+<--- END --->
+
+<-- ADMIN MOCK DATA -- DEFAULT PASS is admin>
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (1, 'Piggy', 'Trussell', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'ptrussell0');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (2, 'Renae', 'Cattermoul', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'rcattermoul1');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (3, 'Derk', 'Nutbrown', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'dnutbrown2');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (4, 'Marie-ann', 'Kellog', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'mkellog3');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (5, 'Renell', 'Dusey', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'rdusey4');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (6, 'Cob', 'Goldsberry', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'cgoldsberry5');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (7, 'Merv', 'Akred', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'makred6');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (8, 'Neville', 'Oldman', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'noldman7');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (9, 'Aeriell', 'MacNeill', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'amacneill8');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (10, 'Zabrina', 'Shuker', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'zshuker9');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (11, 'Arluene', 'Frugier', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'afrugiera');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (12, 'Natalya', 'Guitte', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'nguitteb');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (13, 'Edmon', 'Cashen', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'ecashenc');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (14, 'Kliment', 'Osmar', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'kosmard');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (15, 'Belita', 'Trustey', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'btrusteye');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (16, 'Harriett', 'McIlvoray', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'hmcilvorayf');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (17, 'Junina', 'Bartoshevich', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'jbartoshevichg');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (18, 'Alexi', 'Bensley', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'abensleyh');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (19, 'Godfree', 'Bris', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'gbrisi');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (20, 'Ruttger', 'Leyre', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'rleyrej');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (21, 'Palm', 'Zanetello', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'pzanetellok');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (22, 'Patrick', 'Iorizzo', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'piorizzol');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (23, 'Max', 'Hawlgarth', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'mhawlgarthm');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (24, 'Randy', 'Townsend', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'rtownsendn');
+insert into tbl_admins (id, first_name, last_name, password, user_role, username) values (25, 'Gabie', 'Lawrence', '$2a$12$uIQQWBfaTYlwvzdKyWi2Jukw0vJaE00BS/Rv1y0L87X/ozYss/86q', 'admin', 'glawrenceo');
+<--- END --->
