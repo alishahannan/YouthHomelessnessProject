77610c080ad96f67b12b5414d29554ac26f8956d
bellindtonCayo <79161958+bellindtonCayo@users.noreply.github.com>
2022-10-07 18:50:34 -0400

    Add files via upload
    Actual dependent, housing, and food resources for 21 zip codes in Orlando "The City Beautiful."

---
+++ food_resource_script.sql
@@ -0,0 +1,84 @@
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (43, 'Winter Park', '407-644-7593', 'FL', '2100 Lee Rd', 'https://www.jfsorlando.org/pearlmanfoodpantry', '32789');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (43, 5.0, 'Pearlman Emergency Food Pantry', 'Food Pantry', 43, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (44, 'Orlando', '407-423-4301', 'FL', '626 E Concord St', 'http://myconcordstreet.org', '32803');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (44, 5.0, 'Concord Street COC Emergency Food Pantry', 'Food Pantry', 44, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (45, 'Orlando', '407-298-2056', 'FL', '1310 W Colonial Dr Suite 25-26', 'https://cflhavenofhope.com', '32804');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (45, 5.0, 'Haven Of Hope Food Pantry', 'Food Pantry', 45, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (46, 'Orlando', '407-295-1066', 'FL', '411 Mercy Dr', 'https://www.feedhopenow.org/site/SPageServer/', '32805');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (46, 5.0, 'Second Harvest Food Bank of Central Florida', 'Food Pantry', 46, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (47, 'Orlando', '407-730-8847', 'FL', '2569 E Kaley Ave', 'https://www.cflhope.org/sodo', '32806');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (47, 5.0, 'The Central Florida Hope Center – SoDo', 'Food Pantry', 47, true, false, false);
+
+INSERT INTO_tbl_addresses (id, city, phone, state, street, website, zip) VALUES (48, 'Orlando', '407-658-1818', 'FL', '1771 N Semoran Blvd', 'https://cflcc.org', '32807');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (48, 5.0, 'Semoran Food Pantry', 'Food Distribution Center', 48, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (49, 'Orlando', '407-425-2523', 'FL', '808 W. Central Boulevard', 'https://www.christianservicecenter.org', '32805');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (49, 5.0, 'Christian Service Center', 'Food Pantry', 49, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (50, 'Orlando', '407-295-1002', 'FL', '3702 Mercy Star Ct', 'https://www.feedhopenow.org/site/SPageServer/', '32808');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (50, 5.0, 'Second Harvest Food Bank of Central Florida - Mercy Kitchen', 'Food Pantry', 50, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (51, 'Orlando', '407-295-1923', 'FL', '1000 Bethune Dr', 'https://episcopalsaintjohn.com', '32805');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (51, 5.0, 'Episcopal Church of St. John the Baptist', 'Food Pantry', 51, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (52, 'Orlando', '407-521-7750', 'FL', '5514 Edgewater Dr', 'https://human-crisis-center-orlando.business.site', '32810');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (52, 5.0, 'Human Crisis Center Inc', 'Food Pantry', 52, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (53, 'Orlando', '407-930-7701', 'FL', '701 S Kirkman Rd #713', 'https://icnarelief.org/florida/', '32811');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (53, 5.0, 'ICNA Relief Central Florida', 'Food Pantry', 53, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (54, 'Orlando', '407-277-4945', 'FL', '4605 Curry Ford Rd', 'https://www.firstuccorlando.org', '32812');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (54, 5.0, 'First United Church of Christ', 'Food Pantry', 54, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (55, 'Orlando', '407-849-2226', 'FL', '2740 Old Winter Garden Rd', 'https://iharvestchurch.com', '32805');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (55, 5.0, 'International Harvest', 'Food Pantry', 55, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (56, 'Orlando', '407-482-0600', 'FL', '9837 E Colonial Dr', 'https://samaritanresourcecenter.org', '32817');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (56, 5.0, 'Samaritan Resource Center', 'Food Pantry', 56, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (57, 'Orlando', '321-314-4083', 'FL', '5580 E Grand Street', 'http://www.dnffoodbank.org', '32822');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (57, 5.0, 'DNF Food Bank', 'Food Bank', 57, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (58, 'Orlando', '407-851-6464', 'FL', '653 Wetherbee Road', 'https://tryvivachurch.com', '32824');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (58, 5.0, 'Viva Church - Central Campus', 'Food Pantry', 58, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (59, 'Orlando', '407-650-0774', 'FL', '150 W Michigan St', 'https://uporlando.org', '32806');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (59, 5.0, 'United Against Poverty', 'Food Pantry', 59, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (60, 'Orlando', '407-966-3400', 'FL', '2520 Hansrob Rd', 'https://www.communityunited.org', '32804');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (60, 5.0, 'Community United Outreach', 'Food Pantry', 60, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (61, 'Orlando', '407-949-1382', 'FL', '4091 Seaboard Rd', 'https://www.riseagainsthunger.org', '32808');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (61, 5.0, 'Rise Against Hunger Orlando', 'Food Pantry', 61, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (62, 'Orlando', '407-240-5442', 'FL', '3400 Hunters Creek Blvd', 'https://greenewaychurch.com', '32837');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (62, 5.0, 'Greeneway Church Food Pantry', 'Food Pantry', 62, true, false, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip) VALUES (63, 'Orlando', '407-470-9091', 'FL', '716 W Robinson St.', 'http://www.serenitysgiftoflove.com', '32805');
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUES (63, 5.0, 'Serenitys Grace', 'Food Pantry', 63, true, false, false);
+
---
+++ housing_resource_script.sql
@@ -0,0 +1,145 @@
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (22, 'Winter Park', '407-951-8269', 'FL', '1000 Clay St', 'https://www.fporlandofl.org/', '32789');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (22, 5.0, 'Family Promise of Greater Orlando', 'Homeless Shelters', 22, false, true, false);
+
+#32801
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (23, 'Orlando', '407-423-8581', 'FL', '624 Lexington Ave', 'https://salvationarmyflorida.org/orlando/', '32801');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (23, 5.0, 'Salvation Army Florida', 'Mens Homeless Shelters', 23, false, true, false);
+
+#32803
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (24, 'Orlando', '407-228-1446', 'FL', '911 N Mills Ave', 'https://zebrayouth.org/', '32803');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (24, 4.2, 'Zebra Coalition', 'Affordable housing', 24, false, true, false);
+
+#32804
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (25, 'Orlando', '407-423-8581', 'FL', '440 W Colonial Dr', 'https://salvationarmyflorida.org/orlando/', '32804');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (25, 5.0, 'The Salvation Army Florida', 'Womens and children’s homeless shelter', 25, false, true, false);
+
+
+#32805
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (26, 'Orlando', '407-422-4855', 'FL', '1521 W Washington St', 'https://www.ourm.org/', '32805');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (26, 5.0, 'Orlando Union Rescue Mission', 'Homeless Shelters', 26, false, true, false);
+
+#32806
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (27, 'Orlando', '407-836-7626', 'FL', '1800 E Michigan St', 'https://www.orangecountyfl.net/', '32806');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (27, 3.0, 'Orange County Youth Shelter', 'Youth shelter', 27, false, true, false);
+
+#32807
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (28, 'Orlando', '407-482-0404', 'FL', '5931 E Colonial Dr', 'https://www.covenanthousefl.org/orlando-services', '32807');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (28, 5.0, 'Covenant House Florida', 'Homeless housing and youth social services organization', 28, false, true, false);
+
+#32808
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (29, 'Orlando', '407-294-0123', 'FL', '3200 W Colonial Dr', 'http://www.pathlighthome.org/', '32808');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (29, 4.5, 'Pathlight Home', 'Affordable housing', 29, false, true, false);
+
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (30, 'Orlando', '407-447-5686', 'FL', '1707 Orlando Central Pkwy #350', 'https://www.cflhands.org/home', '32809');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (30, 3.0, 'Hands of Central Florida', 'Social services organization', 30, false, true, false);
+
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (31, 'Orlando', '407-504-0709', 'FL', '5616 Clarcona Ocoee Rd', 'https://supportfainehouse.org/', '32810');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (31, 4.0, 'The Faine House', 'Youth housing', 31, false, true, false);
+
+
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (32, 'Orlando', '407-893-0133', 'FL', '142 E Jackson St', 'https://hsncfl.org', '32811');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (32, 5.0, 'Homeless Services Network of Central Florida', 'Homeless Shelters', 32, false, true, false);
+
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (33, 'Orlando', '407-482-0404', 'FL', '5931 E Colonial Dr', 'https://www.covenanthouse.org/', '32807');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (33, 5.0, 'Covenant House Florida', 'Homeless Shelters', 33, false, true, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (34, 'Winter Park', '407-951-8269', 'FL', '1000 Clay St', 'https://www.fporlandofl.org', '32789');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (34, 5.0, 'Family Promise of Greater Orlando', 'Housing Services', 34, false, true, false);
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (35, 'Orlando', '407-422-4855', 'FL', '1521 W Washington St', 'https://www.ourm.org', '32805');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (35, 5.0, 'Orlando Union Rescue Mission', 'Offers Safe Shelter and Hot Meals', 35, false, true, false);
+
+ 
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (36, 'Orlando', '407-658-1818', 'FL', '1771 N Semoran Blvd', 'https://cflcc.org', '32807');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (36, 3.0, 'Catholic Charities of Central Florida', 'Providing Assistance', 36, false, true, false);
+
+ 
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (37, 'Orlando', '407-316-8174', 'FL', '2020 36th St', 'https://www.houseofhopegb.org', '32839');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (37, 3.2, 'House of Hope', 'Housing Services', 37, false, true, false);
+
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (38, 'Orlando', '407-482-0600', 'FL', '9833 E Colonial Dr', 'https://samaritanresourcecenter.org', '32817');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (38, 4.0, 'Samaritan Resource Center', 'Homeless Resource Center', 38, false, true, false);
+
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (39, 'Orlando', '407-482-0404', 'FL', '5931 E Colonial Dr', 'https://www.covenanthouse.org', '32807');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (39, 4.0, 'Covenant House Florida', 'Homeless Shelter', 39, false, true, false);
+
+
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (40, 'Orlando', '407-426-1250', 'FL', '18 N Terry Ave', 'https://www.coalitionforthehomeless.org/', '32801');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (40, 5.0, 'Coalition for the Homeless', 'Homeless Shelter', 40, false, true, false);
+
+ 
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (41, 'Orlando', '407-480-1657', 'FL', '3200 W Colonial Dr', 'https://www.pathlighthome.org/', '32808');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (41, 4.5, 'Pathlight Home', 'Housing Resource', 41, false, true, false);
+
+#32839
+INSERT INTO tbl_addresses (id, city, phone, state, street, website, zip)
+VALUES (42, 'Orlando', '407-447-3060', 'FL', '4049 S Orange Blossom Trl', 'https://www.pathlighthome.org/our-programs/safe-haven-program/', '32839');
+
+INSERT INTO tbl_resources (id, degree, name, description, address_id, food_resource, housing_resource, dependent_resource)
+VALUE (42, 4.5, 'Safe Haven', 'Homeless Shelter', 42, false, true, false);
